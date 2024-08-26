cc3a01a72cbcff7bfa0851393653b9ac400dc00a
Kamikazi-Deborah <deborahkamikazi9@gmail>
2024-08-26 08:55:19 +0200

    Ex3

---
+++ Exercise 4/Ex3.js
@@ -0,0 +1,12 @@
+const RomansDecoder=(number)=>{
+    let roman = ""; 
+    let numbers = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    let romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ]
+    for (let i = 0; i < numbers.length; ++i) {  
+      while (number >= numbers[i]) {
+        roman = roman + romanNumeral[i];
+        number = number - numbers[i];
+      };
+    };
+    return roman;
+  };
